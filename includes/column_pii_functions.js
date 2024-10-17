0b09d01b5584afed4779ffcb499df92954de71eb
Adi Widjaja <adiwijaya@carion.joonix.net>
2024-10-17 06:16:09 +0000

    dev

--- includes/column_pii_functions.js
+++ includes/column_pii_functions.js
@@ -1,3 +1,20 @@
 function apply_pii(columns){
     return columns;
 }
+
+const TRAFFIC_SOURCE_MEDIUM = {
+  test: `Traffic source medium`
+};
+
+const TRAFFIC_SOURCE_NAME = {
+  traffic_source_name: `Name of traffic source`
+};
+
+
+const ga4_events = {
+  ...TRAFFIC_SOURCE_MEDIUM,
+  ...TRAFFIC_SOURCE_NAME,
+};
+module.exports = {
+  ga4_events
+}
